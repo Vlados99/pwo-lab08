6aca452b9cff81fd2a83c276f9039465015a8cf3
Vladyslav Veshnevskyi <vladyslav.veshnevskyi@infinite.pl>
2022-10-22 18:39:25 +0200

    PWO lab08 - praca z modułami

---
+++ lab08-engine/src/main/java/pwo/seq/FibonacciGenerator.java
@@ -0,0 +1,51 @@
+package pwo.seq;
+
+import java.math.BigDecimal;
+
+public class FibonacciGenerator extends Generator {
+
+    public FibonacciGenerator() {
+        current = new BigDecimal(0);
+        f_1 = new BigDecimal(1);
+        f_2 = new BigDecimal(0);
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        current = new BigDecimal(0);
+        f_1 = new BigDecimal(1);
+        f_2 = new BigDecimal(0);
+    }
+
+    @Override
+    public BigDecimal nextTerm() {
+        if (lastIndex > 1) {
+            current = f_1.add(f_2);
+            f_2 = f_1;
+            f_1 = current;
+        } else if (lastIndex == 1) {
+            current = new BigDecimal(1);
+
+        } else {
+            current = new BigDecimal(0);
+        }
+        lastIndex++;
+        return current;
+    }
+    
+    @Override
+    public BigDecimal previousTerm() {
+        if (lastIndex > 1) {
+            current = f_1.subtract(f_2);
+            f_2 = f_1;
+            f_1 = current;
+        } else if (lastIndex == 1) {
+            current = new BigDecimal(1);
+        } else {
+            current = new BigDecimal(0);
+        }
+        lastIndex--;
+        return current;
+    }
+}
---
+++ lab08-engine/src/main/java/pwo/utils/SequenceGenerator.java
@@ -0,0 +1,14 @@
+package pwo.utils;
+
+import java.math.BigDecimal;
+
+public interface SequenceGenerator {
+
+    void reset();
+
+    BigDecimal nextTerm();
+
+    BigDecimal previousTerm();
+    
+    BigDecimal getTerm(int i);
+}
